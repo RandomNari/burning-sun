72fe19680162096dfc12d1162b620342045c03a2
RandomNari <giotareddragon@gmail.com>
2026-08-06 01:14:42 +0200

    i now got most of the holdergetters and holders over to my mixin and changed "name" to "official" in the accesswidener because the error code said so

--- src/main/java/burning/sun/random/nari/mixin/WorldPresetMixin.java
+++ src/main/java/burning/sun/random/nari/mixin/WorldPresetMixin.java
@@ -1,5 +1,20 @@
 package burning.sun.random.nari.mixin;
 
+import burning.sun.random.nari.BurningSun;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSourceParameterList;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSourceParameterLists;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +27,8 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.presets.WorldPreset;
 import net.minecraft.world.level.levelgen.presets.WorldPresets;
 
+import java.util.Map;
+
 @Mixin(WorldPresets.class)
 public abstract class WorldPresetMixin {
 
@@ -20,5 +37,25 @@ public abstract class WorldPresetMixin {
     @Inject(method = "bootstrap", at = @At("TAIL"))
     private static void createBurnedWorld(final BootstrapContext<WorldPreset> context, CallbackInfo ci) {
 
+        HolderGetter<DimensionType> dimensionTypes = context.lookup(Registries.DIMENSION_TYPE);
+
+        HolderGetter<NoiseGeneratorSettings> noiseSettings = context.lookup(Registries.NOISE_SETTINGS);
+
+        HolderGetter<MultiNoiseBiomeSourceParameterList> biomePresets = context.lookup(Registries.MULTI_NOISE_BIOME_SOURCE_PARAMETER_LIST);
+
+        HolderGetter<Biome> biomes = context.lookup(Registries.BIOME);
+
+        HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+
+        HolderGetter<StructureSet> structureSets = context.lookup(Registries.STRUCTURE_SET);
+
+        Holder<DimensionType> overworldType = dimensionTypes.getOrThrow(BuiltinDimensionTypes.OVERWORLD);
+
+        Holder<NoiseGeneratorSettings> overworldNoise = noiseSettings.getOrThrow(NoiseGeneratorSettings.OVERWORLD);
+
+        Holder.Reference<MultiNoiseBiomeSourceParameterList> overworldBiomePreset = biomePresets.getOrThrow(MultiNoiseBiomeSourceParameterLists.OVERWORLD);
+
     }
 }
+
+
